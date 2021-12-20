842cc38f25fa17373c218fe1c6e23487d25e3221
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2021-12-20 18:48:53 +0600

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// for Each Implementation
 function forEach(arr, cb) {
   for (let i = 0; i < arr.length; i++) {
     arr[i] = cb(arr[i], i, arr);
   }
   return arr;
 }
-
+// map Implementation
 function map(arr, cb) {
   let modify = [];
   for (let i = 0; i < arr.length; i++) {
@@ -12,7 +13,7 @@ function map(arr, cb) {
   }
   return modify;
 }
-
+// filter Implementation
 function filter(arr, cb) {
   let modify = [];
   for (let i = 0; i < arr.length; i++) {
@@ -20,20 +21,50 @@ function filter(arr, cb) {
   }
   return modify;
 }
-
+// reduce Implementation
 function reduce(arr, cb, acc) {
   for (let i = 0; i < arr.length; i++) {
     acc = cb(acc, arr[i]);
   }
   return acc;
 }
+// find Implementation
+function find(arr, cb) {
+  let data = undefined;
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      return arr[i];
+    }
+  }
+  return data;
+}
+// find Index
+function findIndex(arr, cb) {
+  let dataIndex = undefined;
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      return i;
+    }
+  }
+  return dataIndex;
+}
 
 let array = [1, 2, 3, 4, 5];
-let forEachData = forEach(array, (val, index) => val * 2);
+
+let forEachData = forEach(array, (val) => val * 2);
 console.log('forEach  ', forEachData);
-let mapData = map(array, (val, index) => val);
+
+let mapData = map(array, (val) => val);
 console.log('Map ', mapData);
+
 let filterData = filter(array, (val) => val > 2);
 console.log('Filter ', filterData);
+
 let reduceSum = reduce(array, (sum, val) => (sum *= val), 1);
 console.log('Reduce ', reduceSum);
+
+let findData = find(array, (val) => val > 6);
+console.log('Find ', findData);
+
+let findDataIndex = findIndex(array, (val) => val > 6);
+console.log('Find Index ', findDataIndex);
